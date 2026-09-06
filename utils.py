7ba997fd0bd59ca51610969e6e87e8454f980487
Uthman Yishawu <134627561+UthmanTY@users.noreply.github.com>
2026-09-06 21:35:03 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
-
+def get_valid_number(max_number):
+    while True:
+        try:
+            guess = int(input())
+            if guess >= 1 and guess <= max_number:
+                return guess
+            else:
+                print("Invalid number! Please enter a number within the range.")
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+          
 def score_calculator(max_attempt, attempts_used, difficulty_multiplier):
   total = (max_attempt - attempts_used + 1) * difficulty_multiplier
   return total
